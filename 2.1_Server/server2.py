291f42ac18720d7a43a83d15188da1a0b41848ca
student <paulhjwu@gmail.com>
2021-02-03 10:59:30 +0000

    ch

---
+++ 2.1_Server/server2.py
@@ -0,0 +1,165 @@
+import http.server
+import socketserver
+import logging
+from io import BytesIO
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+from urllib import parse
+import math
+import json
+
+import os
+
+####
+
+import tempfile
+import webbrowser
+#import jinja2
+
+###
+
+error_msg = "<html><head></head><body><h1>There is an error</h1></body></html>"
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+    
+    def do_GET(self):
+
+        pwd=os.getcwd()
+
+        if self.path == '/':
+            try:
+                with open(pwd + "/2.1_Server/index4.html") as file:
+                    data = file.read()
+
+                self._set_response()
+                self.wfile.write(bytes(data, "utf8"))
+
+            except:
+                self.wfile.write(bytes(error_msg, "utf8"))
+
+            return
+
+        if '/bmi' in self.path:
+            
+            # Sending an '200 OK' response
+            self.send_response(200)
+
+            # Setting the header
+            self.send_header("Content-type", "text/html")
+
+            # Whenever using 'send_header', you also have to call 'end_headers'
+            self.end_headers()
+
+            query_components = parse_qs(urlparse(self.path).query)
+
+            if 'height' in query_components:
+                height = float(query_components["height"][0])
+            
+            if 'weight' in query_components:
+                weight = float(query_components["weight"][0])
+        
+            bmi = weight / pow(height, 2)
+
+            html = f"<html><head></head><body><h1>The BMI is {bmi}!</h1></body></html>"
+            self.wfile.write(bytes(html, "utf8"))
+        return
+        
+    def do_POST(self):
+
+        bmi = 0
+        
+        if '/bmi-ajax' in self.path:
+            
+            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+            post_data = self.rfile.read(content_length) # <--- Gets the data itself
+            logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",str(self.path), str(self.headers), post_data.decode('utf-8'))
+        
+            self._set_headers()     
+            post_data_str = post_data.decode("utf-8")
+            params = parse_qs(post_data_str)
+
+            weight = float(params['weight'][0])
+            height = float(params['height'][0])
+
+            if params['unit'][0] == 'm':
+                bmi = weight / math.pow(height, 2)
+            else:
+                bmi = weight / math.pow(height/100, 2)
+
+            self.wfile.write(json.dumps({'bmi': bmi}).encode('utf-8'))
+            #self.wfile.write(f'<html><body>ANSWER IS BMI {bmi}, POST request for {self.path} and params {post_data}</body></html>'.encode('utf-8'))
+        
+            # pwd=os.getcwd()
+            # try:
+            #     with open(pwd + "/2.1_Server/resp/index4.html") as file:
+            #         data = file.read()
+
+            #     self._set_response()
+
+            #     # Whenever using 'send_header', you also have to call 'end_headers'
+            #     self.end_headers()
+            #     self.wfile.write(bytes(data, "utf8"))
+            # except:
+            #     self.wfile.write(bytes(error_msg, "utf8"))
+
+        return
+
+        if '/bmi' in self.path:
+            
+            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+            post_data = self.rfile.read(content_length) # <--- Gets the data itself
+            logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",str(self.path), str(self.headers), post_data.decode('utf-8'))
+        
+            #self._set_response()     
+            post_data_str = post_data.decode("utf-8")
+            params = parse_qs(post_data_str)
+
+            weight = float(params['weight'][0])
+            height = float(params['height'][0])
+
+            if params['unit'][0] == 'm':
+                bmi = weight / math.pow(height, 2)
+            else:
+                bmi = weight / math.pow(height/100, 2)
+
+            # self.wfile.write(f'<html><body>POST request for {self.path} and params {post_data}</body></html>'.encode('utf-8'))
+            #self.wfile.write(f'<html><body>ANSWER IS BMI {bmi}, POST request for {self.path} and params {post_data}</body></html>'.encode('utf-8'))
+        
+            # pwd=os.getcwd()
+            # try:
+            #     with open(pwd + "/2.1_Server/resp/index4.html") as file:
+            #         data = file.read()
+
+            #     self._set_response()
+
+            #     # Whenever using 'send_header', you also have to call 'end_headers'
+            #     self.end_headers()
+            #     self.wfile.write(bytes(data, "utf8"))
+            # except:
+            #     self.wfile.write(bytes(error_msg, "utf8"))
+
+        return
+
+
+# Create an object of the above class
+handler_object = MyHttpRequestHandler
+
+PORT = 8000
+my_server = socketserver.TCPServer(("", PORT), handler_object)
+
+def main():
+    my_server.serve_forever()
+
+# Star the server
+if __name__ == "__main__":   
+    my_server.serve_forever()
